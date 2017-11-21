ec77b8209186f9fb4de117e9d95d3fedf2f49bf3
ClumpsyPenguin <seppe318@gmail.com>
2017-11-21 17:01:24 +0100

    added url swagger api

---
+++ Ontime/Backend/OnTimeBackend/Controllers/EmployersController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnTimeBackend.Data;
+using Uber4Cream.Data.DatabaseModels;
+
+namespace OnTimeBackend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Employers")]
+    public class EmployersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Employers
+        [HttpGet]
+        public IEnumerable<Employer> Getemployers()
+        {
+            return _context.employers;
+        }
+
+        // GET: api/Employers/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployer([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var employer = await _context.employers.SingleOrDefaultAsync(m => m.EmployerID == id);
+
+            if (employer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employer);
+        }
+
+        // PUT: api/Employers/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployer([FromRoute] int id, [FromBody] Employer employer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != employer.EmployerID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Employers
+        [HttpPost]
+        public async Task<IActionResult> PostEmployer([FromBody] Employer employer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.employers.Add(employer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmployer", new { id = employer.EmployerID }, employer);
+        }
+
+        // DELETE: api/Employers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployer([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var employer = await _context.employers.SingleOrDefaultAsync(m => m.EmployerID == id);
+            if (employer == null)
+            {
+                return NotFound();
+            }
+
+            _context.employers.Remove(employer);
+            await _context.SaveChangesAsync();
+
+            return Ok(employer);
+        }
+
+        private bool EmployerExists(int id)
+        {
+            return _context.employers.Any(e => e.EmployerID == id);
+        }
+    }
+}
