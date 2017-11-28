978c8f633b82b94ba8f375e8f30a832b8b5fffd9
mo <info@norant.be>
2017-11-28 12:35:06 +0100

    added column reasontitle

--- Ontime/Backend/OnTimeBackend/Migrations/20171128113021_reastontitle.Designer.cs
+++ Ontime/Backend/OnTimeBackend/Migrations/20171128113021_reastontitle.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace OnTimeBackend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171128113021_reastontitle")]
+    partial class reastontitle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Ontime/Frontend/App/app/src/main/java/com/dytstudio/signup/Issues/AddIssue.java
@@ -0,0 +1,19 @@
+package com.dytstudio.signup.Issues;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dytstudio.signup.R;
+
+public class AddIssue extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_issue);
+
+
+
+    }
+}
