4852a3f31b14145058377d6d0e40bed0afdced48
Mo Bouzim <info@norant.be>
2017-12-24 12:15:23 +0100

    locationfromaddress

---
+++ Ontime/Backend/OnTimeBackend/Data/googlemapsapi/GoogleApiResponse.cs
@@ -0,0 +1,100 @@
+﻿
+namespace QuickType
+{
+    using System;
+    using System.Net;
+    using System.Collections.Generic;
+
+    using Newtonsoft.Json;
+
+    public partial class GoogleApi
+    {
+        [JsonProperty("results")]
+        public Result[] Results { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+    }
+
+    public partial class Result
+    {
+        [JsonProperty("address_components")]
+        public AddressComponent[] AddressComponents { get; set; }
+
+        [JsonProperty("formatted_address")]
+        public string FormattedAddress { get; set; }
+
+        [JsonProperty("geometry")]
+        public Geometry Geometry { get; set; }
+
+        [JsonProperty("partial_match")]
+        public bool PartialMatch { get; set; }
+
+        [JsonProperty("place_id")]
+        public string PlaceId { get; set; }
+
+        [JsonProperty("types")]
+        public string[] Types { get; set; }
+    }
+
+    public partial class Geometry
+    {
+        [JsonProperty("location")]
+        public Location Location { get; set; }
+
+        [JsonProperty("location_type")]
+        public string LocationType { get; set; }
+
+        [JsonProperty("viewport")]
+        public Viewport Viewport { get; set; }
+    }
+
+    public partial class Viewport
+    {
+        [JsonProperty("northeast")]
+        public Location Northeast { get; set; }
+
+        [JsonProperty("southwest")]
+        public Location Southwest { get; set; }
+    }
+
+    public partial class Location
+    {
+        [JsonProperty("lat")]
+        public double Lat { get; set; }
+
+        [JsonProperty("lng")]
+        public double Lng { get; set; }
+    }
+
+    public partial class AddressComponent
+    {
+        [JsonProperty("long_name")]
+        public string LongName { get; set; }
+
+        [JsonProperty("short_name")]
+        public string ShortName { get; set; }
+
+        [JsonProperty("types")]
+        public string[] Types { get; set; }
+    }
+
+    public partial class GoogleApi
+    {
+        public static GoogleApi FromJson(string json) => JsonConvert.DeserializeObject<GoogleApi>(json, Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this GoogleApi self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    public class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+        };
+    }
+}
