1abacc78e5de69b79c49753615f87557bb2cfdcc
François Blanpain <blanpainfrancois@gmail.com>
2017-12-26 11:30:28 +0100

    api call issues

--- Ontime/Frontend/OnTimeFrontend/src/app/services/getemployees.service.ts
+++ Ontime/Frontend/OnTimeFrontend/src/app/services/getemployees.service.ts
@@ -4,19 +4,23 @@ import { Observable } from 'rxjs/Observable';
 import {Router} from "@angular/router";
 import {AuthService} from './auth.service';
 
+const headers = new HttpHeaders().set('Authorization', "Bearer " + this.authService.getToken()["access_token"]);
+
 @Injectable()
 export class GetemployeesService {
   constructor(private client : HttpClient, private router : Router, private authService: AuthService) { }
+  
 
     public getAllEmployees(){
-      const headers = new HttpHeaders().set('Authorization', "Bearer " + this.authService.getToken()["access_token"]);
       return this.client.get("http://ontimeapi.azurewebsites.net/api/Employees",{headers:headers});
-
     }
 
     public employeeToEmployer(id:number){
-      const headers = new HttpHeaders().set('Authorization', "Bearer " + this.authService.getToken()["access_token"]);
       return this.client.post("http://ontimeapi.azurewebsites.net/api/Employers/employeetoemployer?id=" + id, {headers:headers});
+    }
+
+    public getAllIssues(){
+      return this.client.get("http://ontimeapi.azurewebsites.net/api/Issues", {headers:headers});
     }
 
     
