4be4f539e562e7e2eee4801b46dc022c97f336df
ClumpsyPenguin <seppe318@gmail.com>
2018-01-09 13:26:22 +0100

    dd

---
+++ Ontime/Frontend/OnTimeFrontend/src/app/ng/ng.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng',
+  templateUrl: './ng.component.html',
+  styleUrls: ['./ng.component.css']
+})
+export class NgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
